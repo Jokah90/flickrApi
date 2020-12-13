c9f9ddeb84a8eb8f610c1ec51d05654f058bca0d
john hansson <johnhansson90@gmail.com>
2020-12-13 12:57:58 +0100

    mediaquerys mobile responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const key = "51444c1c092948d36d530fdf66473531";
 const form = document.querySelector('#input-box');
 const searchTerm = document.querySelector('#search');
-let startBtn = document.getElementById('start-btn');
+let btn = document.getElementById('cta-btn');
+let overlay = document.getElementById('overlay');
+
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -12,7 +15,7 @@ form.addEventListener('submit', e => {
 async function getData(query) {
     try {
 
-        const API_URL = `https://www.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${query}&per_page=2&page=6&format=json&nojsoncallback=1`;
+        const API_URL = `https://www.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${query}&per_page=20&page=6&format=json&nojsoncallback=1`;
         const response = await fetch(API_URL);
         const data = await response.json();
         console.log(data)
@@ -25,37 +28,30 @@ async function getData(query) {
     }
 
 }
-
-let overlay = document.getElementById('overlay');
-startBtn.addEventListener('click', () => {
-    overlay.style.display = 'grid';
-})
-
+ 
 function showPhotos(photos) {
-
     photos.forEach(value => {
         figUrl = `https://farm${value.farm}.staticflickr.com/${value.server}/${value.id}_${value.secret}_m.jpg`;
         console.log(figUrl);
 
         let art = document.createElement('article');
-        startBtn.addEventListener('click', () => {
-    
-        })
+        
         let template = `
             <figure>
                 <img src="${figUrl}" alt="${value.title}"></img>
             </figure>
+        `;
             
-            `;
-            
-
-
         art.insertAdjacentHTML('afterbegin', template)
-        document.querySelector('main').appendChild(art)
+            
+            document.querySelector('main').appendChild(art)
     });
-    
 }
 
+btn.addEventListener('click', () => {
+    overlay.style.display = ('grid');
+    overlay.classList.add('animate-overlay')
+})
 
 
 
