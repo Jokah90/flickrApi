54aa670dc4a0e2349e252203755905598b96ec0a
john hansson <johnhansson90@gmail.com>
2020-12-16 14:39:05 +0100

    new changes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', e => {
 async function getData(query) {
     try {
         overlay.innerHTML = null; //Clearar tidigare resultat
-        const API_URL = `https://www.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${query}&sort=relevance&per_page=6&page=6&format=json&nojsoncallback=1`;
+        const API_URL = `https://www.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${query}&sort=relevance&per_page=1&page=6&format=json&nojsoncallback=1`;
         const response = await fetch(API_URL);
         const data = await response.json();
         console.log(data)
@@ -32,7 +32,9 @@ function showPhotos(photos) {
     photos.forEach(value => {
         figUrl = `https://farm${value.farm}.staticflickr.com/${value.server}/${value.id}_${value.secret}_m.jpg`;
         console.log(figUrl);
-        
+
+      
+  
 
         let template = `
             <article>
@@ -42,24 +44,18 @@ function showPhotos(photos) {
             </article>
         `;
         overlay.innerHTML += template; //La in article i template så man slipper ha en extra kodrad och skapar dem på detta vis för att slippa alla AdjacentHTML
-        let figure = document.getElementById('fig');
-        console.log(figure);
-
-        figure.addEventListener('click', () => {
-            console.log('sfads');
-            showImage();
-        })
      
+    
+        
     });
 
     let exitBtn = `<button id="exit" class="exit-button"><p>X</p></button>`; //Skapa upp exitknapp
     overlay.innerHTML += exitBtn; //Släng med den i HTML
     document.getElementById('exit').addEventListener('click', exit); //Lägg på eventLyssnare för att nå funktionen
-   
-}
-
-function showImage(fig) {
-    console.log(fig);
+    let img = document.getElementById('img')
+    img.addEventListener('click', () => {
+        showImage(img);
+    });
 }
 
 // lyssnar efter ett click och animerar en overlay, grid i js
@@ -71,6 +67,10 @@ btn.addEventListener('click', () => {
 //Func som tar bort className från overlay, så att det inte längre syns
 const exit = () => {
     overlay.classList.remove('animate-overlay'); 
+}
+
+let showImage = (overlay) => {
+    overlay.classList.add('animate-overlay'); 
 }
 
 
